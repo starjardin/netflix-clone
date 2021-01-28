3f9d2c2c0e997eeec4179f59213d76cbe2770982
Tantely Andrianarivola <tantely.and@onja.org>
2021-01-28 14:58:13 +0300

    added dotenv file

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import { App } from './app';
 import { FirebaseContext } from './context/firebase';
 import { GlobalStyles } from './global-styles';
+import dotenv from 'dotenv'
 
+dotenv.config()
 //import { seedDatabase } from './seed'
 
 const config = {
   databaseUrl: "",
-  apiKey: "AIzaSyCy_vG5YzBC0Yii1uIcD6jjK7z2i2g8J1k",
-  authDomain: "netflix-onja-tanteli.firebaseapp.com",
-  projectId: "netflix-onja-tanteli",
-  storageBucket: "netflix-onja-tanteli.appspot.com",
-  messagingSenderId: "817700823508",
-  appId: "1:817700823508:web:0ced800eba5896addaf641"
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BACKET,
+  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
+  appId: process.env.FB_APP_ID
 };
 
 const firebase = window.firebase.initializeApp(config)
